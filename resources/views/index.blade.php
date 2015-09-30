c0844939b97caab816b8a3f982b14002772b434c
Thomas Roseman <roseman.tom@gmail.com>
2015-09-30 18:18:47 -0400

    Added npm files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Things Professors Say</title>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Things Professors Say</div>
+    </div>
+</div>
+</body>
+</html>
